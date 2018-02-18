d6930d361d4cf23bfd2c8d0a5072d3ce9f2cadc3
Neha Prasad <nehaprasad14@gmail.com>
2018-02-17 21:36:42 -0600

    code uploaded

---
+++ lambda/user-profile/index.js
@@ -0,0 +1,56 @@
+/**
+ * Required Env Vars:
+ * AUTH0_DOMAIN
+ */
+'use strict';
+
+var request = require('request');
+
+function generateResponse(status, message){
+    return {
+		statusCode: status,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({'message':message})
+    }
+}
+
+exports.handler = function(event, context, callback){
+    console.log(event);
+    var idToken = event.headers.Authorization;
+    var accessToken = event.queryStringParameters.accessToken;
+
+    if (!idToken) {
+      var response = generateResponse(400, 'ID token not found');
+
+    	callback(null, response);
+    	return;
+    }
+
+    if (!accessToken) {
+        var response = generateResponse(400, 'AccessToken not found');
+  
+        callback(null, response);
+        return;
+    }
+
+    var options = {
+        url: 'https://' +  process.env.AUTH0_DOMAIN + '/userinfo',
+        method: 'POST',
+        json: true,
+        headers: {
+            'Authorization': `Bearer ${accessToken}`
+        }
+    };
+
+    request(options, function(error, response, body){
+        if (!error && response.statusCode === 200) {
+            var response = generateResponse(200, body);
+
+            callback(null, response);
+        } else {
+            var response = generateResponse(400, error);
+
+            callback(null, response);
+        }
+    });
+};
---
+++ website/js/config.js
@@ -0,0 +1,8 @@
+var configConstants = {
+    auth0: {
+        domain: 'add domain',
+        clientId: 'add clientId',
+	},
+	apiBaseUrl: 'add apiBaseUrl',
+	firebaseUrl: 'add firebaseUrl'
+};
